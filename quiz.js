7eb9c509bd3b8e9185be70dabb03f00ea68001c6
Kevin Johnson <kevin.michael805@gmail.com>
2021-07-01 13:06:48 -0600

    clean up.

--- quiz.js
+++ quiz.js
@@ -89,7 +89,29 @@ let score = 0;
 
 loadQuiz();
 
+function loadQuiz() {
+    deselectAnswers();
 
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
 
 function deselectAnswers() {
     answerEls.forEach((answerEl) => {
